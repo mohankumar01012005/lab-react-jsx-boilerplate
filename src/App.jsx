9e6f1eab999eb7cdb2cb235dbb319b1b7fa4ace6
mohankumar01012005 <kumar.mohan@kalvium.community>
2023-12-06 17:43:19 +0530

    react-jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,29 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+  let image_arr=imageData()
+  console.log(image_arr)
+  return (
+   <div>
+     <h1>Kalvium Gallery</h1>
+    {/* <div className='parent'>
+      <img src={image_arr[0].img} className='image'/>
+      <img src={image_arr[0].img} className='image'/>
+      <img src={image_arr[0].img} className='image'/>
+      <img src={image_arr[0].img} className='image'/>
+    </div> */}
+    <div className='parent'>
+    {
+      image_arr.map(elem=>{
+        return(
+          <img className='image' src={elem.img}/>
+        )
+      })
+    }
+
+    </div>
+   </div>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,29 @@ export default class AppClass extends Component{
   }
 
   // code here
+
+  render (){
+    let image_arr=this.imageData()
+    return (
+      <div>
+        <h1>Kalvium Gallery</h1>
+       {/* <div className='parent'>
+         <img src={image_arr[0].img} className='image'/>
+         <img src={image_arr[0].img} className='image'/>
+         <img src={image_arr[0].img} className='image'/>
+         <img src={image_arr[0].img} className='image'/>
+       </div> */}
+       <div className='parent'>
+       {
+         image_arr.map(elem=>{
+           return(
+             <img className='image' src={elem.img}/>
+           )
+         })
+       }
+   
+       </div>
+      </div>
+     )
+  }
 }
